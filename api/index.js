7799482010b0749624ef46a1b8b75a458fa302ab
lucaslb1 <lucasbattelle@gmail.com>
2021-07-11 09:56:08 -0700

    removed db connection info

--- api/index.js
+++ api/index.js
@@ -8,11 +8,11 @@ const app = express()
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'password',
-    database: 'leaguetime'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 })
 
 try {
